f58c51dfe340e01dd33b73305edbcc8ea6a8dca9
Matthias Geier <Matthias.Geier@gmail.com>
2013-10-21 17:31:23 +0200

    Documentation for apf::BlockParameter

--- apf/misc.h
+++ apf/misc.h
@@ -48,16 +48,28 @@ class NonCopyable
     //@}
 };
 
+/// Hold current and old value of any type.
+/// A new value can be assigned with operator=().
+/// The current and old value can be obtained with get() and get_old(),
+/// respectively.
+/// To find out if the value has changed with the last assignment, use
+/// changed().
+/// @note Using operator=() is the only way to change the value, all other
+///   member functions are constant.
+/// @tparam T The contained type.
 template<typename T>
 class BlockParameter
 {
   public:
+    /// Constructor. Any arguments are forwarded to both old and current value.
     template<typename... Args>
     explicit BlockParameter(Args&&... args)
       : _current{args...}
       , _old{std::forward<Args>(args)...}
     {}
 
+    /// Assignment operator.
+    /// As a side effect, the old value is updated to the former current value.
     template<typename Arg>
     T& operator=(Arg&& arg)
     {
@@ -65,11 +77,13 @@ class BlockParameter
       return _current = std::forward<Arg>(arg);
     }
 
-    const T& get() const { return _current; }
-    const T& get_old() const { return _old; }
+    const T& get() const { return _current; }  ///< Get current value.
+    const T& get_old() const { return _old; }  ///< Get old value.
 
+    /// Conversion operator. For avoiding to call get() all the time.
     operator const T&() const { return this->get(); }
 
+    /// Check if value has changed at last assignment.
     bool changed() const { return this->get() != this->get_old(); }
 
   private:
